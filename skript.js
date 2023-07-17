797d1e1a370055fb8a18f37a888cbe2240ffeb83
Anastasia Litvinchuk <lytvynchukmark@gmail.com>
2023-07-17 12:53:49 +0300

    Homework-18

---
+++ skript.js
@@ -0,0 +1,68 @@
+// 1)
+let Celsius = 30;
+
+let Fahrenheit = (( Celsius + 40) * 1.8) - 40;
+
+console.log("Celsius -", Celsius, "Fahrenheit -", Fahrenheit);
+
+// 2)
+
+let days = 31;
+
+let hours = days * 24;
+
+let minutes = hours * 60;
+
+console.log("days -", days, "hours -", hours, "minutes -", minutes);
+
+// 3)
+
+let helth = 100;
+
+let energy = 100;
+
+helth = helth - 20;
+
+energy = energy - 30;
+
+console.log("helth -", helth, "energy -", energy);
+
+// 4)
+
+let sum = 100;
+
+let discaunt = 10;
+
+let total = sum - (sum * discaunt / 100);
+
+console.log("sum -", sum, "discaunt -", discaunt, "total -", total);
+
+// 5)
+
+let number = 54.234;
+
+console.log(Math.floor(number));
+
+// 6)
+let number2 = 54.480;
+
+console.log(parseFloat(number2));
+
+// 7)
+let number3 = "54.480";
+
+console.log(parseInt(number3));
+
+// 8)
+let number4 = 9;
+
+console.log(Math.sqrt(number4));
+
+// 9)
+
+let number5 = 54.234;
+
+let string6 = "54.234";
+
+console.log("number5 - ", number5.toString())
+console.log("string6 - ", parseInt(string6))
